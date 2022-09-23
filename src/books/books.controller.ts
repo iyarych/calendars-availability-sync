c095297da808a2cfe4aee77a6b527ba6398336dd
Ivan Yarych <yarych@gmail.com>
2022-09-23 17:41:45 +0100

    Books CRUD example from Digital Ocean

---
+++ src/books/books.controller.ts
@@ -0,0 +1,32 @@
+import { Controller, Get, Param, Post, Body, Delete } from '@nestjs/common';
+import { BooksService } from './books.service';
+import { CreateBookDTO } from './dto/create-book.dto';
+
+@Controller('books')
+export class BooksController {
+  constructor(private booksService: BooksService) {}
+
+  @Post()
+  async addBook(@Body() createBookDTO: CreateBookDTO) {
+    const book = await this.booksService.addBook(createBookDTO);
+    return book;
+  }
+
+  @Get()
+  async getBooks() {
+    const books = await this.booksService.getBooks();
+    return books;
+  }
+
+  @Get(':bookID')
+  async getBook(@Param('bookID') bookID) {
+    const book = await this.booksService.getBook(bookID);
+    return book;
+  }
+
+  @Delete(':bookID')
+  async deleteBook(@Param('bookID') bookID) {
+    const books = await this.booksService.deleteBook(bookID);
+    return books;
+  }
+}
